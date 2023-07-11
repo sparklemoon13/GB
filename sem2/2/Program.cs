41e48c8028ea5b90fd507e2155541bcaf807315e
Anastasia <asbryansk@yandex.ru>
2023-07-12 00:28:27 +0300

    sem2.task2

---
+++ sem2/2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//что третьей цифры нет.
+
+Console.WriteLine("Input your number");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 100)
+{
+   int var = number;
+   while (var>1000)
+   {
+      var = number/10;
+      number = number/10;
+   }
+    int dig = var % 10;
+    Console.WriteLine(dig);
+}
+else Console.WriteLine("The number doesn't have the third digit."); 
