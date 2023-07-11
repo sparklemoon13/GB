98809d9efe98756f0e7492a68d6863729d381647
Anastasia <asbryansk@yandex.ru>
2023-07-11 22:24:40 +0300

    sem2.task1

---
+++ sem2/1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 10: Напишите программу, которая принимает 
+//на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Input three-digit number");
+int Num = Convert.ToInt32(Console.ReadLine());
+if (Num >= 100 && Num <1000)
+{
+ int NumA = Num / 100;
+ int NumB = (Num - NumA * 100) / 10;
+ Console.WriteLine(NumB);
+}
+else
+{
+  Console.WriteLine("Your number is not three-digit."); 
+}
