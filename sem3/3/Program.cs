322d07349d65e907b8e15cb2cae76023b3a85a49
Anastasia <asbryansk@yandex.ru>
2023-07-14 00:10:23 +0300

    sem3.task3

---
+++ sem3/3/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Input number N"); 
+int N = Convert.ToInt32(Console.ReadLine());
+int CubesN (int N)
+{
+    int number = 1;
+    int cube = 0;
+    while (number<=N)
+    {
+    cube = number*number*number;
+    number++;
+    Console.Write($" {cube} ");
+    }
+    return cube;
+    
+}
+if (N>0)
+{
+    int CubesNumbers = CubesN(N);
+}
+else
+{
+Console.WriteLine("Input number N > 0!"); 
+}
