878358cded4eb566c8526de1907c7fb768f517ed
Anastasia <asbryansk@yandex.ru>
2023-07-17 22:24:39 +0300

    sem4.task3

---
+++ sem4/3/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив 
+//из 8 элементов и выводит их на экран.
+
+int[] RandomArray(int lenght)
+{
+    int[] result = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        result[i] = new Random().Next(0,10);
+    }
+    return result;
+}
+void PrintArray(int[] arrayToPrint)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write(arrayToPrint[i]);
+        if (i < arrayToPrint.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write(" ]");
+}
+int[] RandArray = RandomArray(8);
+PrintArray(RandArray);
