f94d22e029b93abae82c7300cd329c7b0af8492a
Anastasia <asbryansk@yandex.ru>
2023-07-17 22:00:22 +0300

    sem4.task2

---
+++ sem4/2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+
+int SumOfNumbers(int Number)
+{
+    int Sum = 0;
+    while (Number > 0)
+    {
+        Sum = Sum + Number % 10;
+        Number = Number / 10;
+    }
+    return Sum;
+}
+
+Console.WriteLine("Input your Number");
+int Number = Convert.ToInt32(Console.ReadLine());
+int Sum = SumOfNumbers(Number);
+Console.WriteLine($"The sum of digits of your Number is {Sum}");
