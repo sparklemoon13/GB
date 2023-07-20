3d2882e669508d9c0a47b79591b1e14aa74d9590
Anastasia <asbryansk@yandex.ru>
2023-07-20 22:36:03 +0300

    sem5.task1

---
+++ sem5/1/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 34: Задайте массив заполненный случайными 
+//положительными трёхзначными числами. Напишите программу, 
+//которая покажет количество чётных чисел в массиве.
+
+int[] RandomArray()
+{
+    int size = new Random().Next(7, 30);
+    int[] randomArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        randomArray[i] = new Random().Next(100, 1000);
+    }
+    return randomArray;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int CountEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"The count of even number in your array is {count}");
+    return count;
+}
+int[] arrayRand = RandomArray();
+PrintArray(arrayRand);
+CountEvenNumbers(arrayRand);
