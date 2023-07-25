d5cfc205e9d80c4d39abf8cecaa7fbcd9ed675c7
Anastasia <asbryansk@yandex.ru>
2023-07-25 21:57:51 +0300

    sem6.task2

---
+++ sem6/2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double[] CreateValuesMassive()
+{
+    double[] newArray = new double[4];
+    Console.WriteLine("Input k1");
+    newArray[0] = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Input b1");
+    newArray[1] = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Input k2");
+    newArray[2] = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Input b2");
+    newArray[3] = Convert.ToDouble(Console.ReadLine());
+    return newArray;
+}
+
+void FindPoint(double[] array)
+{
+    //double XCoord = (b2 - b1)/(k1 - k2);//
+    double XCoord = (array[3] - array[1]) / (array[0] - array[2]);
+    //double YCoord = k1*XCoord + b1// 
+    double YCoord = array[0] * XCoord + array[1];
+    Console.WriteLine($"Well, I guess the coords for intersection is:{XCoord:f3} ; {YCoord:f3}");
+}
+double[] massiveValues = CreateValuesMassive();
+FindPoint(massiveValues);
