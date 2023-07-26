16e25a9eddb0f9f693aec4255a30d2e11100451e
Anastasia <asbryansk@yandex.ru>
2023-07-26 19:51:23 +0300

    sem7.task1

---
+++ sem7/1/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+
+//47
+double[,] CreateArray()
+{
+    Console.WriteLine("Enter a number of rows for your array :");
+    int rowsOfArray = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter a number of collums for your array :");
+    int collsOfArray = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Minimum value of your array ? ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Maximum value of your array ? ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+
+    double[,] newArray = new double[rowsOfArray, collsOfArray];
+    for (int i = 0; i < rowsOfArray; i++)
+    {
+        for (int j = 0; j < collsOfArray; j++)
+        {
+            newArray[i, j] = Math.Round((new Random().Next(minValue, maxValue) + new Random().NextDouble()), 2);
+        }
+    }
+    return newArray;
+}
+
+    void Print2dArray(double[,] array2d)
+    {
+        for (int i = 0; i < array2d.GetLength(0); i++)
+        {
+            for (int j = 0; j < array2d.GetLength(1); j++)
+            {
+                Console.Write(array2d[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+    double[,] newArray2d = CreateArray();
+    Print2dArray(newArray2d);
